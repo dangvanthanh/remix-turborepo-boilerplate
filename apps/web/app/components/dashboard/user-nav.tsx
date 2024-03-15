935cf3e149fae6046178de9560e892db9729fe12
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-03-15 19:58:59 +0700

    feat: update avatars

--- apps/web/app/components/dashboard/user-nav.tsx
+++ apps/web/app/components/dashboard/user-nav.tsx
@@ -17,7 +17,7 @@ export function UserNav() {
 			<DropdownMenuTrigger asChild>
 				<Button variant="ghost" className="relative h-8 w-8 rounded-full">
 					<Avatar className="h-8 w-8">
-						<AvatarImage src="/avatars/01.png" alt="@remix" />
+						<AvatarImage src="https://ui.shadcn.com/avatars/01.png" alt="@remix" />
 						<AvatarFallback>SC</AvatarFallback>
 					</Avatar>
 				</Button>
