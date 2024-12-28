528c509391c69d52ac1e09e6c03481d277e740d2
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-12-29 00:19:49 +0700

    chore: #2 - React Router 7 and Lingui

--- packages/i18n/src/lingui.server.ts
+++ packages/i18n/src/lingui.server.ts
@@ -1,19 +1,19 @@
-import { createCookie } from 'react-router';
-import config from '../lingui.config';
-import { ReactRouterLingui } from './react-router.server';
+import { createCookie } from 'react-router'
+import config from '../lingui.config'
+import { ReactRouterLingui } from './react-router.server'
 
 export const localeCookie = createCookie('lng', {
-  path: '/',
-  sameSite: 'lax',
-  secure: process.env.NODE_ENV === 'production',
-  httpOnly: true,
-});
+	path: '/',
+	sameSite: 'lax',
+	secure: process.env.NODE_ENV === 'production',
+	httpOnly: true,
+})
 
 export const linguiServer = new ReactRouterLingui({
-  detection: {
-    supportedLanguages: config.locales,
-    fallbackLanguage:
-      (!!config.fallbackLocales && config.fallbackLocales?.default) || 'en',
-    cookie: localeCookie,
-  },
-});
+	detection: {
+		supportedLanguages: config.locales,
+		fallbackLanguage:
+			(!!config.fallbackLocales && config.fallbackLocales?.default) || 'en',
+		cookie: localeCookie,
+	},
+})
