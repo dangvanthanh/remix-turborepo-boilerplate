dce0eea9dee55fe67a22337df407a97f3143480e
Dang Van Thanh <dangvanthanh@dangthanh.org>
2025-01-24 21:43:31 +0700

    chore: update tailwind.config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,8 +1,6 @@
-import baseConfig from '@repo/ui/tailwind.config'
 import type { Config } from 'tailwindcss'
 
 export default {
 	content: ['./app/**/*.{ts,tsx}', '../../packages/ui/src/**/*.{ts,tsx}'],
-	presets: [baseConfig],
 	theme: {},
 } satisfies Config
